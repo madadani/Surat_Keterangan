96610feb0739de16747fa4bc4583c8eeb15e10da
madadani <wahyuramadanii27@gmail.com>
2026-02-09 10:47:43 +0700

    perbaikan fitur filter data pendaftar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,11 @@ class AppServiceProvider extends ServiceProvider
             if (str_starts_with($appUrl, 'https')) {
                 \Illuminate\Support\Facades\URL::forceScheme('https');
             }
+
+            // Fix Livewire update route for subdirectories on server
+            \Livewire\Livewire::setUpdateRoute(function ($handle) {
+                return \Illuminate\Support\Facades\Route::post('/livewire/message/{name}', $handle);
+            });
         }
     }
 }
