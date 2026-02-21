b90523783563a10003663c3292224206fe6ec03e
madadani <wahyuramadanii27@gmail.com>
2026-02-21 13:48:38 +0700

    format rtf poli gigi

--- resources/views/admin/rtf/body_gigi.blade.php
+++ resources/views/admin/rtf/body_gigi.blade.php
@@ -11,22 +11,33 @@ No. RM\tab : \b {!! ($pendaftar->no_rm ?? '................') !!}\b0\par\par
 \pard\sl276\slmult1\ql Telah dilakukan pemeriksaan kesehatan gigi dan mulut pada tanggal \b {!! $pada_tanggal !!}\b0
 dengan hasil dan tindakan sebagai berikut:\par\par
 \b A. PEMERIKSAAN MULUT DAN GIGI (ODONTOGRAM)\b0\par
-\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
-@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
-\pard\intbl\qc\b RAHANG\cell STATUS\cell
-@foreach(['M3', 'M2', 'M1', 'P2', 'P1', 'C', 'I2', 'I1', 'I1', 'I2', 'C', 'P1', 'P2', 'M1', 'M2', 'M3'] as $tooth){!! $tooth !!}\cell
-@endforeach\b0\row
-\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
-@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
-\pard\intbl\qc ATAS\cell {!! $surat->mcu_data['odontogram_atas_status'] ?? '-' !!}\cell
-@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_atas_g' . $i] ?? '' !!}\cell @endfor\row
-\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
-@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
-\pard\intbl\qc BAWAH\cell {!! $surat->mcu_data['odontogram_bawah_status'] ?? '-' !!}\cell
-@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_bawah_g' . $i] ?? '' !!}\cell @endfor\row
-\par
-\li360\b Hasil Pemeriksaan :\b0\space {!! ($surat->hasil_pemeriksaan ?? '-') !!}\par\par
-\b 2. Perawatan yang Telah Dilakukan\b0\par
+@php
+    $toothNames = ['M3', 'M2', 'M1', 'P2', 'P1', 'C', 'I2', 'I1', 'I1', 'I2', 'C', 'P1', 'P2', 'M1', 'M2', 'M3'];
+    // Border shorthand
+    $bd = "\\clbrdrt\\brdrs\\brdrw10\\clbrdrl\\brdrs\\brdrw10\\clbrdrb\\brdrs\\brdrw10\\clbrdrr\\brdrs\\brdrw10";
+    // Kolom: Rahang(900) + Status(1300) + 16 gigi x 450 = 7200 => total 9400
+    $odonRow = "\\trowd\\trgaph50\\trleft360{$bd}\\cellx900{$bd}\\cellx1700";
+    for ($t = 1; $t <= 16; $t++) {
+        $odonRow .= "{$bd}\\cellx" . (1700 + ($t * 481));
+    }
+@endphp
+{{-- Header Odontogram --}}
+{!! $odonRow !!}
+\pard\intbl\qc\fs16\b RAHANG\cell STATUS\cell
+@foreach($toothNames as $tooth){!! $tooth !!}\cell
+@endforeach\b0\fs20\row
+{{-- Baris ATAS --}}
+{!! $odonRow !!}
+\pard\intbl\qc\fs16 ATAS\cell {!! $surat->mcu_data['odontogram_atas_status'] ?? 'Normal' !!}\cell
+@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_atas_g' . $i] ?? '-' !!}\cell @endfor\fs20\row
+{{-- Baris BAWAH --}}
+{!! $odonRow !!}
+\pard\intbl\qc\fs16 BAWAH\cell {!! $surat->mcu_data['odontogram_bawah_status'] ?? 'Normal' !!}\cell
+@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_bawah_g' . $i] ?? '-' !!}\cell @endfor\fs20\row
+\pard\sl276\slmult1\ql\par
+
+\li360\b Hasil Pemeriksaan :\b0 {!! ($surat->hasil_pemeriksaan ?? '-') !!}\par\par
+\b B. PERAWATAN YANG TELAH DILAKUKAN\b0\par
 @php
     $tindakan_saved = explode(', ', $surat->tindakan_gigi ?? '');
     $tindakan_list = [
@@ -41,7 +52,7 @@ dengan hasil dan tindakan sebagai berikut:\par\par
     \li360 [ {!! in_array($t, $tindakan_saved) ? 'X' : '  ' !!} ] {!! $t !!}\par
 @endforeach
 \par
-\b 3. Rencana / Pemeriksaan Lanjutan\b0\par
+\b C. RENCANA / PEMERIKSAAN LANJUTAN\b0\par
 \li360 Pasien disarankan untuk menjalani pemeriksaan dan/atau perawatan lanjutan, berupa:\par
 \li360 Kontrol ulang pada tanggal :
 {!! $surat->kontrol_ulang ? \Carbon\Carbon::parse($surat->kontrol_ulang)->translatedFormat('d F Y') : '................' !!}\par
