211f013bbad2b0fc84a721240fd0693c75c17684
madadani <wahyuramadanii27@gmail.com>
2026-02-21 10:27:33 +0700

    format rtf gigi

--- resources/views/admin/rtf/body_gigi.blade.php
+++ resources/views/admin/rtf/body_gigi.blade.php
@@ -7,12 +7,25 @@ Instansi\tab : \b Poliklinik Gigi dan Mulut RSUD dr. Soeratno Gemolong\b0\par\pa
 \pard\sl276\slmult1\ql\li360\tx3000 Nama Pasien\tab : \b {!! $pendaftar->nama_lengkap !!}\b0\par
 Umur / JK\tab : \b {!! $umur !!} Tahun / {!! $pendaftar->jenis_kelamin !!}\b0\par
 Alamat\tab : \b {!! $pendaftar->alamat !!}\b0\par
-No. RM\tab : \b {!! ($pendaftar->no_rm ?? '........................................') !!}\b0\par\par
+No. RM\tab : \b {!! ($pendaftar->no_rm ?? '................') !!}\b0\par\par
 \pard\sl276\slmult1\ql Telah dilakukan pemeriksaan kesehatan gigi dan mulut pada tanggal \b {!! $pada_tanggal !!}\b0
 dengan hasil dan tindakan sebagai berikut:\par\par
-\b 1. Hasil Pemeriksaan\b0\par
-\li360 Keadaan gigi dan jaringan sekitar gigi telah dilakukan pemeriksaan klinis :\par
-\li360 {!! ($surat->hasil_pemeriksaan ?? '-') !!}\par\par
+\b A. PEMERIKSAAN MULUT DAN GIGI (ODONTOGRAM)\b0\par
+\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
+@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
+\pard\intbl\qc\b RAHANG\cell STATUS\cell
+@foreach(['M3', 'M2', 'M1', 'P2', 'P1', 'C', 'I2', 'I1', 'I1', 'I2', 'C', 'P1', 'P2', 'M1', 'M2', 'M3'] as $tooth){!! $tooth !!}\cell
+@endforeach\b0\row
+\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
+@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
+\pard\intbl\qc ATAS\cell {!! $surat->mcu_data['odontogram_atas_status'] ?? '-' !!}\cell
+@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_atas_g' . $i] ?? '' !!}\cell @endfor\row
+\trowd\trgaph108\trleft360\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx1200\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx2200
+@for($i = 1; $i <= 16; $i++)\clbrdrt\brdrs\brdrw10\clbrdrl\brdrs\brdrw10\clbrdrb\brdrs\brdrw10\clbrdrr\brdrs\brdrw10\cellx{!! 2200 + ($i * 450) !!}@endfor
+\pard\intbl\qc BAWAH\cell {!! $surat->mcu_data['odontogram_bawah_status'] ?? '-' !!}\cell
+@for($i = 1; $i <= 16; $i++){!! $surat->mcu_data['odontogram_bawah_g' . $i] ?? '' !!}\cell @endfor\row
+\par
+\li360\b Hasil Pemeriksaan :\b0\space {!! ($surat->hasil_pemeriksaan ?? '-') !!}\par\par
 \b 2. Perawatan yang Telah Dilakukan\b0\par
 @php
     $tindakan_saved = explode(', ', $surat->tindakan_gigi ?? '');
@@ -25,23 +38,13 @@ dengan hasil dan tindakan sebagai berikut:\par\par
     ];
 @endphp
 @foreach($tindakan_list as $t)
-    \li360 [ {!! in_array($t, $tindakan_saved) ? '\u10003?' : ' ' !!} ] {!! $t !!}\par
+    \li360 [ {!! in_array($t, $tindakan_saved) ? 'X' : '  ' !!} ] {!! $t !!}\par
 @endforeach
 \par
 \b 3. Rencana / Pemeriksaan Lanjutan\b0\par
 \li360 Pasien disarankan untuk menjalani pemeriksaan dan/atau perawatan lanjutan, berupa:\par
-\li360 Kontrol ulang pada tanggal ................................\par
+\li360 Kontrol ulang pada tanggal :
+{!! $surat->kontrol_ulang ? \Carbon\Carbon::parse($surat->kontrol_ulang)->translatedFormat('d F Y') : '................' !!}\par
 \li360 Perawatan lanjutan : \b {!! ($surat->saran ?? '-') !!}\b0\par\par
 \pard\sl276\slmult1\ql Demikian surat keterangan ini dibuat dengan sebenar-benarnya untuk dapat dipergunakan sebagaimana
-mestinya.\par\par
-\trowd\trgaph108\trleft-108\clvertalt\cellx5000\clvertalt\cellx10000
-\pard\intbl\qc Mengetahui\par Kepala Bidang Pelayanan RSUD dr. Soeratno\par Gemolong Kabupaten Sragen\cell
-\pard\intbl\qc Sragen, {!! $tanggal_cetak !!}\par Dokter Gigi Pemeriksa\cell\row
-\trowd\trgaph108\trleft-108\clvertalt\cellx5000\clvertalt\cellx10000
-\pard\intbl\qc @if($isMayaMengetahui) {!! $ttdMaya !!}\par @else \par\par\par\par\par @endif
-\pard\intbl\qc\b\ul {!! trim($m_nama) !!}\ulnone\b0\par
-\pard\intbl\qc NIP. {!! trim($m_nip) !!}\cell
-\pard\intbl\qc @if($isMayaPemeriksa) {!! $ttdMaya !!}\par @else \par\par\par\par @endif
-\pard\intbl\qc\b\ul{\expndtw-15 {!! trim($surat->dokter->nama_dokter) !!}\expndtw0}\ulnone\b0\par
-\pard\intbl\qc {!! ($surat->identitas_pemeriksa ?? 'NIP') !!}.
-{!! trim(preg_replace('/\s+/', ' ', ($surat->identitas_pemeriksa === 'SIP' ? ($surat->dokter->sip ?? '-') : ($surat->dokter->nip ?? '-')))) !!}\cell\row\pard\par
+mestinya @if($surat->keperluan) untuk \b {!! strtoupper($surat->keperluan) !!}\b0 @endif.\par\par
